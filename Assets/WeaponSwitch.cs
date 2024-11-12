5ae82ac4566fafbb0a9b1fe192b3cf647acba7a6
skveroo <101058520+skveroo@users.noreply.github.com>
2024-11-12 20:51:54 +0100

    Basic game features
    Added basic game features:
    Basic shooting system
    Health system
    Gun switch system

---
+++ Assets/WeaponSwitch.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+public class WeaponSwitch : MonoBehaviour
+{
+    private InputMenager controls;
+    [SerializeField] private bool isAbleToPickUp;
+    [SerializeField] private GameObject currentWeapon;
+    [SerializeField] private GameObject pickupWeapon;
+
+
+    private void Awake()
+    {
+        controls = new InputMenager();
+        weaponDetection();
+        controls.Player.Take.performed += ctx => takeWeapon();
+    }
+    void weaponDetection()
+    {
+        GameObject[] weapons = GameObject.FindGameObjectsWithTag("Weapon");
+        foreach (GameObject obj in weapons)
+        {
+            if (obj.activeSelf)
+            {
+                currentWeapon = obj;
+            }
+        }
+    }
+    void takeWeapon()
+    {
+        if (isAbleToPickUp)
+        {
+            GameObject[] allWeaponModels = GameObject.FindGameObjectsWithTag("weaponSpotGun");
+            currentWeapon.SetActive(false);
+            pickupWeapon.SetActive(true);
+            pickupWeapon = currentWeapon;
+            weaponDetection();
+            foreach (GameObject weapon in allWeaponModels)
+            {
+                MeshRenderer weaponMeshRenderer = weapon.GetComponent<MeshRenderer>();
+                if (weapon.name == pickupWeapon.name)
+                {
+                    weaponMeshRenderer.enabled = true;
+                }
+                else
+                {
+                    weaponMeshRenderer.enabled = false;
+                }
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isAbleToPickUp = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isAbleToPickUp = false;
+        }
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
