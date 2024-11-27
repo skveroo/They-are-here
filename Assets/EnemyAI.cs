8744292e8e16b91c1c49ba54022eab14bceba829
BartZaj <zajaczek5155@gmail.com>
2024-11-27 13:19:49 +0100

    Ai navigation enemies tiktaks and dummy

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent agent;  // Komponent NavMeshAgent
+    private Transform player;   // Transform gracza
+
+    void Start()
+    {
+        // Pobranie komponentu NavMeshAgent
+        agent = GetComponent<NavMeshAgent>();
+
+        // Znalezienie gracza po tagu "Player"
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            player = playerObject.transform;
+        }
+        else
+        {
+            Debug.LogError("Brak obiektu gracza z tagiem 'Player'");
+        }
+    }
+
+    void Update()
+    {
+        // Jeœli gracz istnieje, ustaw jego pozycjê jako cel
+        if (player != null)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
